5c11a9e2e7cc0371607acdc0f114a21febfd0b2f
GuerreroWelinton <guerrerozamora213@gmail.com>
2023-10-11 21:04:17 -0500

    srerver2

---
+++ server2/src/app.module.ts
@@ -0,0 +1,58 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { CiudadModule } from './ciudad/ciudad.module';
+import { RolesModule } from './roles/roles.module';
+import { PersonaModule } from './persona/persona.module';
+import { DepartamentoModule } from './departamento/departamento.module';
+import { DocumentoModule } from './documento/documento.module';
+import { DepartamentoUsuarioModule } from './departamento_usuario/departamento_usuario.module';
+import { SolicitudBajaModule } from './solicitud_baja/solicitud_baja.module';
+import { ProcesoDeterminadoModule } from './proceso_determinado/proceso_determinado.module';
+import { FlujoProcesoModule } from './flujo_proceso/flujo_proceso.module';
+import { ProcesoModule } from './proceso/proceso.module';
+import { TipoFlujoProcesoModule } from './tipo_flujo_proceso/tipo_flujo_proceso.module';
+import { OpcionesModule } from './opciones/opciones.module';
+import config from './config';
+import { Documento, DocumentoSchema } from './documento/schema/documento.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:['.develop.env'],
+      isGlobal:true
+    }),
+    //MongooseModule.forRoot('mongodb+srv://jjrz1350517171:1350517171@cluster0.ezxkxg9.mongodb.net/'),
+    //MongooseModule.forRoot('mongodb://mongo:27017/cbm'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get<string>('MONGO_USERNAME')}:${configService.get<string>('MONGO_PASSWORD')}@cluster0.ezxkxg9.mongodb.net/`,
+        // Más configuraciones de Mongoose si es necesario
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([{ name: Documento.name, schema: DocumentoSchema }]),
+
+    AuthModule,
+    UserModule,
+    CiudadModule,
+    RolesModule,
+    PersonaModule,
+    DepartamentoModule,
+    DocumentoModule,
+    DepartamentoUsuarioModule,
+    SolicitudBajaModule,
+    ProcesoDeterminadoModule,
+    FlujoProcesoModule,
+    ProcesoModule,
+    TipoFlujoProcesoModule,
+    OpcionesModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ server2/src/auth/jwt.constants.ts
@@ -0,0 +1,14 @@
+export const jwtConstants = {
+
+    secret : 'Cbm'
+
+}
+
+// import { ConfigService } from '@nestjs/config';
+
+// export const jwtConstants = {
+//   get secret(): string {
+//     const configService = new ConfigService(); // Crea una instancia de ConfigService
+//     return configService.get<string>('JWT_SECRET'); // Obtiene la clave JWT desde las variables de entorno
+//   },
+// };
